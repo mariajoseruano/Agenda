a9e71fb89afe82166115da009251322a4716ad81
Usuario <Usuario@Portatil3>
2018-12-02 08:32:04 +0100

    Ejercicio 7

---
+++ src/main/java/org/iesalandalus/programacion/agenda/Agenda.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.iesalandalus.programacion.agenda;
+
+/**
+ *
+ * @author Usuario
+ * 
+ */
+//Ejercicio 7.Crear clase Agenda con los atributos.
+public class Agenda {
+    
+    
+    private int numContactos;
+    private static int MAX_CONTACTOS;
+    
+}
